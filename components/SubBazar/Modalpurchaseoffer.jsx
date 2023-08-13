60c28e78d0076d1068d63d9ec0a529c509103bb9
Maedeh Asadi <maedeh.a7879@gmail.com>
2023-08-13 17:39:45 +0330

    end:)

--- components/SubBazar/Modalpurchaseoffer.jsx
+++ components/SubBazar/Modalpurchaseoffer.jsx
@@ -13,10 +13,11 @@ export default function Modalpurchaseoffer() {
         totalprice: "  جمع قیمت خرید",
         walletprice: "موجودی کیف پول ",
         price: "۴,350,000,000",
-        price2:'65,000,000'
+        price2:'65,000,000',
         obligatio: 'وجه التزام خرید صاب',
         price0: "0",
-        payable: " مبلغ قابل پرداخت "
+        payable: " مبلغ قابل پرداخت ",
+        checkbox: "در صورت برنده شدن بایست مبلغ قابل پرداخت را تا ۲۴ ساعت پرداخت نمایید در صورت عدم پرداخت مبلغ وجه التزام از کیف پول کسر خواهد شد."
     };
     return (
         <>
@@ -32,7 +33,7 @@ export default function Modalpurchaseoffer() {
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl min-w-[600px]">
+                        <div className="relative w-auto my-6 mx-auto  max-w-[600px]">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
@@ -41,7 +42,7 @@ export default function Modalpurchaseoffer() {
                                         className="p-1  bg-transparent border-0 text-black  float-left text-3xl leading-none font-semibold outline-none focus:outline-none"
                                         onClick={() => setShowModal(false)}
                                     >
-                                        <span className="bg-transparent text-red  h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                        <span className="bg-transparent text-gray900  h-6 w-6 text-2xl block outline-none focus:outline-none">
                                             ×
                                         </span>
                                     </button>
@@ -52,7 +53,7 @@ export default function Modalpurchaseoffer() {
                                 </div>
                                 {/*body*/}
                                 <div className="relative p-6 flex-auto w-full">
-                                    <div className="flex flex-col  bg-sky rounded-md  py-[18px]">
+                                    <div className="flex flex-col  bg-sky rounded-md  py-[18px] px-10">
                                         <div className="grid grid-cols-12 gap-2 ">
                                             <div className="col-span-6">
                                                 <div className="flex flex-col items-center justify-around relative
@@ -71,31 +72,49 @@ export default function Modalpurchaseoffer() {
                                             </div>
 
                                         </div>
-                                        <div className="">
-                                            <div className="">
-                                                <span className=""></span>
-                                                <IRT/>
+                                        <div className="flex flex-col  border-t border-solid border-gray700 pt-5 mt-4">
+                                            <div className=" flex flex-row items-center justify-between mb-[14px]">
+                                                <span className="font-medium text-gray900 text-base">{offerText.totalprice}</span>
+                                                <IRT amount={offerText.price} className2={'text-blue900 font-medium text-xl'}/>
                                             </div>
+                                            <div className=" flex flex-row items-center justify-between mb-[14px]">
+                                                <span className="font-medium text-gray900 text-base">{offerText.walletprice}</span>
+                                                <IRT amount={offerText.price} className2={'text-blue900 font-medium text-xl'}/>
+                                            </div>
+                                            <div className=" flex flex-row items-center justify-between mb-[14px]">
+                                                <span className="font-medium text-gray900 text-base">{offerText.obligatio}</span>
+                                                <IRT amount={offerText.price} className2={'text-blue900 font-medium text-xl'}/>
+                                            </div>
+                                            <div className=" flex flex-row items-center justify-between mb-[14px]">
+                                                <span className="font-medium text-gray900 text-base">{offerText.payable}</span>
+                                                <IRT amount={offerText.price} className2={'text-blue900 font-medium text-xl'}/>
+                                            </div>
+
                                         </div>
+                                    </div>
+                                    <div className="">
+
+                                        <div class="flex items-center mb-4">
+                                            <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                                <label for="default-checkbox" class="ml-2 text-sm font-medium text-blue900 mx-4">{offerText.checkbox} </label>
+                                        </div>
+
+
                                     </div>
 
                                    
                                 </div>
                                 {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                <div className="flex   p-6 rounded-b">
+
                                     <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="bg-emerald-500 text-white bg-blue800 
+                                        active:bg-emerald-600 font-bold uppercase text-sm px-6  w-full
+                                        py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
-                                        Close
-                                    </button>
-                                    <button
-                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Save Changes
+                                        تایید می‌کنم
                                     </button>
                                 </div>
                             </div>
